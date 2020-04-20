c90e0d6e45942a229fd12404f878899c9e8d88e6
YUHE WANG <yuhewang.ustc@gmail.com>
2020-04-19 22:36:13 -0700

    Add clang-format check for project src files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-#include "vec3.h"
 #include "ray.h"
+#include "vec3.h"
 
-int main() {
+int main()
+{
     const int image_width = 200;
     const int image_height = 100;
 
@@ -13,9 +14,11 @@ int main() {
 
     myfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height-1; j >= 0; --j) {
+    for (int j = image_height - 1; j >= 0; --j)
+    {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
+        for (int i = 0; i < image_width; ++i)
+        {
             vec3 color(double(i) / image_width, double(j) / image_height, 0.2);
             color.write_color(myfile);
         }
